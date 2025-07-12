9325f779e179090eaa40cf46a32f38ba04c11a50
Nereu Jr <nnvljr86@gmail.com>
2025-07-12 07:59:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -99,7 +99,7 @@ const TimeCard = () => {
 }
 
 export default function Dashboard() {
-  const { links, isLoaded: areLinksLoaded, handleEditLink, handleDeleteLink, reorderLinks } = useLinks();
+  const { links, isLoaded: areLinksLoaded, handleEditLink, handleDeleteLink, reorderLinks, handleAddNewLink } = useLinks();
 
   const [draggedItem, setDraggedItem] = React.useState<LinkItem | null>(null);
   const [dragOverItem, setDragOverItem] = React.useState<LinkItem | null>(null);
@@ -316,7 +316,7 @@ export default function Dashboard() {
               </p>
               <button
                  className="mt-4 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
-                 onClick={() => useLinks.getState().handleAddNewLink()}
+                 onClick={handleAddNewLink}
               >
                 Adicionar Primeiro Link
               </button>
